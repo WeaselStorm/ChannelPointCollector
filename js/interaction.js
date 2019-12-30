654dc15037a0061f09ef6f07e0278227c8fc0ac4
WeaselStizzorm <amschoening@gmail.com>
2019-12-29 21:55:57 -0600

    initial commit

---
+++ js/interaction.js
@@ -0,0 +1,14 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function whatever() {
+    while (true) {
+        await sleep(2000)
+        if (document.getElementsByClassName("tw-button tw-button--success tw-interactive").length > 0) {
+            document.getElementsByClassName("tw-button tw-button--success tw-interactive")[0].click()
+        }
+    }
+}
+
+whatever();
