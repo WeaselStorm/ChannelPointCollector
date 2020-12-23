2086662038d2a8ab74b63600c5de6f444a88f983
WeaselStorm <amschoening@gmail.com>
2020-12-22 19:26:29 -0600

    Update interaction.js

--- js/interaction.js
+++ js/interaction.js
@@ -5,8 +5,8 @@ function sleep(ms) {
 async function whatever() {
     while (true) {
         await sleep(2000)
-        if (document.getElementsByClassName("tw-button tw-button--success tw-interactive").length > 0) {
-            document.getElementsByClassName("tw-button tw-button--success tw-interactive")[0].click()
+        if (document.getElementsByClassName("tw-button tw-button--success").length > 0) {
+            document.getElementsByClassName("tw-button tw-button--success")[0].click()
         }
     }
 }
