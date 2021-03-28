86727effaf149c2a63c05665561fdab369e09303
WeaselStizzorm <amschoening@gmail.com>
2021-03-28 15:34:40 -0500

    manifest refresh and new bug fixes

--- js/interaction.js
+++ js/interaction.js
@@ -2,35 +2,46 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const CHANNEL_POINT_AREA_CLASS = "community-points-summary";
+const CHANNEL_POINT_SUMMARY_CLASS = "community-points-summary";
 
-const CLAIM_CHAT_CONTAINER = 'div[data-test-selector="chat-private-callout-queue__callout-container"]';
+const CHANNEL_POINT_ICON_QUERY = 'div.claimable-bonus__icon';
 
-const CHAT_TRAY_CLASSES = "chat-input-tray__open";
+const CLAIM_CHAT_CONTAINER_QUERY = 'div[data-test-selector="chat-private-callout-queue__callout-container"]';
+
+const CLAIM_BUTTON_TEXT = "Claim";
+
+const CHAT_TRAY_CLASSES = "div.chat-input-tray__open";
+
+const CLOSE_BUTTON_QUERY = 'button[aria-label="Close"]'
 
 async function whatever() {
     while (true) {
         await sleep(2000);
-        let channelPointArea = document.getElementsByClassName(CHANNEL_POINT_AREA_CLASS);
-        if (channelPointArea[0].getElementsByTagName('button').length > 1) {
-            try {
-                channelPointArea[0].getElementsByTagName('button')[1].click();
-            } catch (e) {
-                console.log('Error in channelPoint collection: ' + e.message)
+        try {
+            let channelPointArea = document.getElementsByClassName(CHANNEL_POINT_SUMMARY_CLASS);
+            if (channelPointArea.length && channelPointArea[0].querySelectorAll(CHANNEL_POINT_ICON_QUERY)) {
+                channelPointArea[0].querySelector(CHANNEL_POINT_ICON_QUERY).click();
             }
-        }
-        
-        let claimButtonContainers = document.querySelectorAll(CLAIM_CHAT_CONTAINER);
-        if (claimButtonContainers.length) {
-            try {
-                claimButtonContainers[0].getElementsByTagName('button')[0].click();
-                await sleep(1500);
-                let chatTray = document.getElementsByClassName(CHAT_TRAY_CLASSES);
-                chatTray[0].getElementsByTagName('button')[0].click();
-            } catch (e) {
-                console.log('Error in drop collection: ' + e.message)
+        } catch (e) { }
+
+        try {
+            let claimButtonContainers = document.querySelectorAll(CLAIM_CHAT_CONTAINER_QUERY);
+            if (claimButtonContainers.length && claimButtonContainers[0].getElementsByTagName('button').length) {
+                let claimButtons = claimButtonContainers[0].getElementsByTagName('button')
+                for (let claimButton of claimButtons) {
+                    if (claimButton.innerText.trim() === CLAIM_BUTTON_TEXT) {
+                        claimButton.click()
+                    }
+                }
             }
-        }
+        } catch (e) { }
+
+        try {
+            let chatTray = document.getElementsByClassName(CHAT_TRAY_CLASSES);
+            if (chatTray.length && chatTray[0].innerText.includes('Claimed!') && chatTray[0].querySelectorAll(CLOSE_BUTTON_QUERY).length) {
+                chatTray[0].querySelector(CLOSE_BUTTON_QUERY).click();
+            }
+        } catch (e) { }
     }
 }
 
