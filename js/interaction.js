e5b33702f0b60d82a58a7422e0a35f1f0bb6b53c
Aaron <amschoening@gmail.com>
2021-12-22 22:57:12 -0600

    fix channel point collection

--- js/interaction.js
+++ js/interaction.js
@@ -4,6 +4,8 @@ const CHANNEL_POINT_SUMMARY_QUERY = "div.community-points-summary";
 
 const CHANNEL_POINT_ICON_QUERY = 'div.claimable-bonus__icon';
 
+const BONUS_BUTTON_QUERY =' div.tw-transition button'
+
 const CLAIM_CHAT_CONTAINER_QUERY = 'div[data-test-selector="chat-private-callout-queue__callout-container"]';
 
 const CHAT_RULES_ACKNOWLEDGEMENT_QUERY = 'button[data-test-selector="chat-rules-ok-button"]';
@@ -12,8 +14,9 @@ const CHAT_TRAY_QUERY = 'div.chat-input-tray__open';
 
 function claimRewards() {
     if (!!document.querySelector("video")) {
-        const bonusButton = document.querySelector([CHAT_ROOM_QUERY, CHANNEL_POINT_SUMMARY_QUERY, CHANNEL_POINT_ICON_QUERY].join(' '));
-        if (bonusButton) {
+        const bonusButtonIcon = document.querySelector([CHAT_ROOM_QUERY, CHANNEL_POINT_SUMMARY_QUERY, CHANNEL_POINT_ICON_QUERY].join(' '));
+        const bonusButton = document.querySelector([CHAT_ROOM_QUERY, CHANNEL_POINT_SUMMARY_QUERY, BONUS_BUTTON_QUERY].join(' '));
+        if (bonusButtonIcon && bonusButton) {
             bonusButton.click();
         }
         const rewardClaimButton = document.querySelector([CLAIM_CHAT_CONTAINER_QUERY, 'button'].join(' '));
